dec09b9f683a81c3d8eabef607315aa786717cb6
b87carter <56475172+b87carter@users.noreply.github.com>
2026-06-27 22:37:53 -0500

    finish opening sequence

--- src/main_menu.c
+++ src/main_menu.c
@@ -1381,7 +1381,7 @@ static void Task_NewGameCarterSpeechSub_InitPokeBall(u8 taskId)
     gSprites[spriteId].invisible = FALSE;
     gSprites[spriteId].data[0] = 0;
 
-    CreatePokeballSpriteToReleaseMon(spriteId, gSprites[spriteId].oam.paletteNum, 112, 58, 0, 0, 32, PALETTES_BG, SPECIES_LOTAD);
+    CreatePokeballSpriteToReleaseMon(spriteId, gSprites[spriteId].oam.paletteNum, 112, 58, 0, 0, 32, PALETTES_BG, SPECIES_WOOPER);
     gTasks[taskId].func = Task_NewGameCarterSpeechSub_WaitForLotad;
     gTasks[sCarterSpeechMainTaskId].tTimer = 0;
 }
@@ -1928,7 +1928,7 @@ static void SpriteCB_MovePlayerDownWhileShrinking(struct Sprite *sprite)
 
 static u8 NewGameCarterSpeech_CreateLotadSprite(u8 x, u8 y)
 {
-    return CreateMonPicSprite_Affine(SPECIES_LOTAD, SHINY_ODDS, 0, MON_PIC_AFFINE_FRONT, x, y, 14, TAG_NONE);
+    return CreateMonPicSprite_Affine(SPECIES_WOOPER, SHINY_ODDS, 0, MON_PIC_AFFINE_FRONT, x, y, 14, TAG_NONE);
 }
 
 static void AddCarterSpeechObjects(u8 taskId)
